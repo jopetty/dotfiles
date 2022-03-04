5956e41335aa64123c37ae8fcb5e2ce98d2bbf13
jopetty <research@jacksonpetty.org>
2022-03-04 14:47:39 -0500

    Add more macOS settings

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -36,3 +36,19 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Show the main window when launching Activity Monitor
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Use AirDrop over every interface. srsly this should be a default.
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Show the ~/Library folder.
+chflags nohidden ~/Library
+
+# Hide Safari's bookmark bar.
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
